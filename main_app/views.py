729a5323081a80e22836689a74573775e537ba2c
jose <jose@ellaware.com>
2017-03-17 00:11:03 +0530

    before migration

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    name  = 'Gold Nugget'
-    value = 1000
-    context = {'treasure_name':name,'treasure_val':value}
+    # name  = 'Gold Nugget'
+    # value = 1000
+    context= dict()
+    context['treasures'] = {'name':"jose",'value':500, 'material':'gold','location':'india In','img_url':'jose.jpg'}
     return render(request,'index.html',context)
